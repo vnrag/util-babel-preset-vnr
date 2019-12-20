0da9906c8e52269a4a8832ff39d7a9e3130fb66a
Jason Nickel <jni@vnr.de>
2019-12-20 17:20:04 +0100

    Fix export as function

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
-module.exports = {
-    presets: [
+module.exports = (api, options = {}) => {
+    const {
+        targets,
+        useBuiltIns = 'entry',
+        corejs = {
+            version: 3,
+            proposals: true
+        }
+    } = options;
+
+    const presets = [
         [
             '@babel/env',
             {
-                useBuiltIns: 'entry',
-                corejs: {
-                    version: 3,
-                    proposals: true
-                }
+                targets,
+                useBuiltIns,
+                corejs
             }
         ],
         '@babel/typescript',
         '@babel/react'
-    ],
-    plugins: [
+    ];
+
+    const plugins = [
         '@babel/proposal-class-properties',
         '@babel/proposal-object-rest-spread',
         '@babel/syntax-dynamic-import',
         '@babel/transform-react-jsx'
-    ]
+    ];
+
+    return {
+        presets: presets,
+        plugins: plugins
+    };
 };
