d32d583c64e69ba9f9e7e383dd2900ae85a30bb2
Jason Nickel <jni@vnr.de>
2019-12-20 14:04:15 +0100

    Init

---
+++ index.js
@@ -0,0 +1,22 @@
+export default {
+    presets: [
+        [
+            '@babel/env',
+            {
+                useBuiltIns: 'entry',
+                corejs: {
+                    version: 3,
+                    proposals: true
+                }
+            }
+        ],
+        '@babel/typescript',
+        '@babel/react'
+    ],
+    plugins: [
+        '@babel/proposal-class-properties',
+        '@babel/proposal-object-rest-spread',
+        '@babel/syntax-dynamic-import',
+        '@babel/transform-react-jsx'
+    ]
+};
